3b56f850f37306ef9e7edba10405b64a6714a5c5
Isaac Garcia <garciaalejoisaac@gmail.com>
2024-05-28 15:00:15 -0500

    new

---
+++ ejercicio4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ejercicio4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Area;
+        String Figura;
+
+        System.out.println("Bienvenido a calcular figuras geométricas");
+        System.out.println("1.El area de un cuadrado");
+        System.out.println("2.El area de un triangulo");
+        System.out.println("3.El area de un circulo");
+
+    }
+}
